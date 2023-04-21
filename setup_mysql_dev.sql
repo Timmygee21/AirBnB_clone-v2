8647b5bc3d934d11ca1da6cdc2edecaba462c054
MamaKbubu <maboelatk@gmail.com>
2023-04-20 19:01:53 -0700

    updating sqldev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
-/* Creating a new datatbase called hbnb_dev_db
-* Adding privileges on the database for hbnb_test database */
+-- Creating hbnb_test database
+-- Add Privileges for hbnb_test database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
