51a680ff85fc5ee337623e275a0a2c90378a9d8a
Timmygee21 <saytimmy@gmail.com>
2023-04-23 16:50:46 +0100

    Add blank line after import

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage improt DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from os import getnv
 
 
-if getnv("HBNB_TYPE_STORAGE") == "db":
-    storage = DBStorage()
-else:
-    storage = FileStorage()
-    storage.reload()
+storage = FileStorage()
+storage.reload()
